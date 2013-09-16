05b6d0fb6ed2d114ca76df4d4b31a8f69e03e407
student <nugaev_razer_94@mail.ru>
2013-09-16 15:01:28 +0400

    Leha schinok

---
+++ untitled/src/TheChosenOneClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: student
+ * Date: 16.09.13
+ * Time: 14:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class TheChosenOneClass {
+    public static void main(String[] args)
+    {
+
+    }
+
+}
